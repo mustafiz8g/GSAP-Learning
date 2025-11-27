c5b00a525cb3b68b498c002bbae8588782962ade
mustafiz8g <mustafizur8g@gmail.com>
2025-11-28 00:21:28 +0600

    complete gsap3 express

--- GSAP 3 EXPRESS/project1.js
+++ GSAP 3 EXPRESS/project1.js
@@ -18,17 +18,35 @@
 
 // let tl = gsap.timeline({repeat: 1, yoyo :true});
 // let tl = gsap.timeline( {defaults:{scale: 4}});
-let tl = gsap.timeline( {defaults:{ease: 'back' ,opacity: 0}});
+// let tl = gsap.timeline( {defaults:{ease: 'back' ,opacity: 0}});
 
-tl.from("#demo", {ease: 'linear'})
-  .from("h1", {duration: 1 ,x: 80})
-  .from("h2", {duration: 1 ,x : -80}, "<") 
-  .from("p", {y:30}, "-=0.3")
-  .from("button", { y: 50}, '-=0.4')
-  .from("#items  > g", {
-    transformOrigin: '50% 50%',
-    scale: 0,
-    stagger: 0.1,
+// tl.from("#demo", {ease: 'linear'})
+//   .from("h1", {duration: 1 ,x: 80})
+//   .from("h2", {duration: 1 ,x : -80}, "<") 
+//   .from("p", {y:30}, "-=0.3")
+//   .from("button", { y: 50}, '-=0.4')
+//   .from("#items  > g", {
+//     transformOrigin: '50% 50%',
+//     scale: 0,
+//     stagger: 0.1,
    
-  }, "-=0.3");
+//   }, "-=0.3");
   
+
+
+//  
+let tl = gsap.timeline({repeat: 4, yoyo: true, defaults: {opacity: 0, ease: 'back'}});
+
+function init() {
+  tl.from("#demo", {ease: "linear", autoAlpha: 0})
+    .from("h1", {x:80, duration:1})
+    .from("h2", {x:-80, duration:1}, "<")
+    .from("p", {y:30}, "-=0.2")
+    .from("button", {y:50}, "-=0.4") 
+    .from("#items > g", {scale:0, transformOrigin:"50% 50%", stagger:0.1}, "-=0.5")
+  
+}
+
+window.addEventListener("load", function(event){
+  init(); // do stuff
+})
